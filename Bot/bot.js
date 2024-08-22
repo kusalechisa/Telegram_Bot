f759f8d87fdf8a5ffce69f67d9ad6cc91e78e1f3
kusa lachisa <kusalechisac@gmail.com>
2024-08-22 20:13:04 +0300

    save

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "7444271355:AAGO5uwQrxJ0JE5AyP-OvjUeNe-UtT4GlKo";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://kusarestaurantbot.netlify.app/";
+const web_link = "https://ketirestaurant.onrender.com/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome to KETI Restaurant:", {
